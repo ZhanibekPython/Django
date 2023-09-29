9ecdadd748c1fab388fa45bc41848050f3ebc8a2
Zhanibek <astic1@mail.ru>
2023-09-30 00:32:01 +0600

    Sem#1_Done

--- Seminar_1/Homework/views.py
+++ Seminar_1/Homework/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+def start(request):
+    return HttpResponse('Welcome to my main page!')
--- Seminar_1/Seminar_1/urls.py
+++ Seminar_1/Seminar_1/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Tasks_and_HW.urls'))
+    path('', include('Tasks_and_HW.urls')),
+    path('', include('Homework.urls')),
 ]
